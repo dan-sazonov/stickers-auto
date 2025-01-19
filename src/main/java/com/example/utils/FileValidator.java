d70a225c3db82df8330ad734a5629624d1a69d4b
Dan Sazonov <p.294803@gmail.com>
2025-01-19 07:35:38 +0300

    feat: update stickerpack mode

--- src/main/java/com/example/utils/FileValidator.java
+++ src/main/java/com/example/utils/FileValidator.java
@@ -1,10 +1,11 @@
 package com.example.utils;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class FileValidator {
-
     public static boolean isValidDirectory(String path) {
         File directory = new File(path);
         return directory.exists() && directory.isDirectory();
@@ -14,5 +15,22 @@ public class FileValidator {
         String[] validExtensions = {".png", ".jpg", ".jpeg"};
         return file.isFile() && Arrays.stream(validExtensions).anyMatch(file.getName().toLowerCase()::endsWith)
                 && !EmojiExtractor.extractEmojisFromFileName(file.getName()).isEmpty();
+    }
+
+    public static List<File> getValidImageFiles(String directory) {
+        List<File> validFiles = new ArrayList<>();
+        File dir = new File(directory);
+
+        if (isValidDirectory(directory)) {
+            File[] files = dir.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (isValidImageFile(file)) {
+                        validFiles.add(file);
+                    }
+                }
+            }
+        }
+        return validFiles;
     }
 }
